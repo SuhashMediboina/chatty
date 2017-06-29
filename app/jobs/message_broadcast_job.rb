3645dce21d4ac6b321a81edb2d28e9bcb9a74b69
Suhash Mediboina <msuhash1987@gmail.com>
2017-06-29 00:36:00 +0000

    first commit

---
+++ app/jobs/message_broadcast_job.rb
@@ -0,0 +1,14 @@
+class MessageBroacastJob < ApplicationJob
+    queue_as :default
+    
+    def perform(message)
+        ActionCable.server.broadcast "chat_rooms_#{message.chat_room.id}_channel",
+                                     message: render_message(message)
+    end
+    
+    private
+    
+    def render_message(message)
+        MessageController.render partial: 'messages/message', locals: { message:message }
+    end
+end
